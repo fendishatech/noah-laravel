98b532f9e55bf6f9a80d0a701f96a1526e01db45
fendishatech <liyukibir@gmail.com>
2023-08-10 08:37:39 -0700

    members dashboard

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
-    public function home()
+    public function index()
     {
         return view("members.index");
     }
--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -6,6 +6,28 @@
 
 @section('content')
     <div class="min-h-screen">
-        <h1 class="mt-[64px] text-center text-green-700 text-4xl">Hello Member</h1>
+        <main class="px-6 flex flex-col gap-4">
+            @if (Session::has('member'))
+                <h1 class="mt-[64px] text-start text-green-700 text-4xl my-4">Hello
+                    {{ Session::get('member')['first_name'] }}
+                </h1>
+            @endif
+            {{-- Lots --}}
+            <section class="px-6 py-4 bg-gray-200 rounded-md">
+                <h1 class="text-3xl font-bold">Lots</h1>
+            </section>
+            {{-- Deposit --}}
+            <section class="px-6 py-4 bg-gray-200 rounded-md">
+                <h1 class="text-3xl font-bold">Lots</h1>
+            </section>
+            {{-- Loan --}}
+            <section class="px-6 py-4 bg-gray-200 rounded-md">
+                <h1 class="text-3xl font-bold">Lots</h1>
+            </section>
+            {{-- Payment --}}
+            <section class="px-6 py-4 bg-gray-200 rounded-md">
+                <h1 class="text-3xl font-bold">Lots</h1>
+            </section>
+        </main>
     </div>
 @endsection
